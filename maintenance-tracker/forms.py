bbaf4174038f925e05f94eb3a7a5d6beef14bdbb
Brian Whiteley <brianwhiteleybw@gmail.com>
2021-01-12 14:25:06 -0600

    add vehicle form

--- maintenance-tracker/forms.py
+++ maintenance-tracker/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, IntegerField
 from wtforms.validators import DataRequired, Email, Length
 
 
@@ -14,3 +14,11 @@ class LoginForm(FlaskForm):
     email = StringField(label='Email', validators=[DataRequired(), Email()])
     password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
     submit = SubmitField(label='Log In')
+
+
+class AddVehicleForm(FlaskForm):
+    name = StringField(label='Name')
+    year = IntegerField(label="Year")
+    make = StringField(label="Make")
+    model = StringField(label="Model", validators=[DataRequired()])
+    submit = SubmitField(label="Add")
--- maintenance-tracker/models.py
+++ maintenance-tracker/models.py
@@ -9,4 +9,26 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(128))
     name = db.Column(db.String(50))
+    vehicles = db.relationship('Vehicle')
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    vehicle_id = db.Column(db.Integer, db.ForeignKey('vehicle.id'))
+    name = db.Column(db.String(50))
+    date = db.Column(db.Date)
+    mileage = db.Column(db.Integer)
+
+
+class Vehicle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30))
+    year = db.Column(db.Integer)
+    make = db.Column(db.String(40))
+    model = db.Column(db.String(50))
+    tasks = db.relationship("Task")
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+
 
