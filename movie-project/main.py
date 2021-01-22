82a892494de02338b2cfcea35ecc98ba350bf675
Brian Whiteley <brianwhiteleybw@gmail.com>
2021-01-21 19:41:46 -0600

    initial

---
+++ movie-project/main.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
+
+Bootstrap(app)
+db = SQLAlchemy()
+db.init_app(app)
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    year = db.Column(db.Integer)
+    description = db.Column(db.String)
+    rating = db.Column(db.Float)
+    ranking = db.Column(db.Integer)
+    review = db.Column(db.String)
+    img_url = db.Column(db.String)
+
+
+with app.app_context():
+    db.create_all()
+
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
